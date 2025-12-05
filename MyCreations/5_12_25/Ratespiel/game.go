f56ee9e852c0bd1ab302a716d42f8e43cd9cd276
Philip <philip_sturm@outlook.de>
2025-12-05 14:23:22 +0100

    Ratespiel mit random number aber ohne tips

---
+++ MyCreations/5_12_25/Ratespiel/game.go
@@ -0,0 +1,32 @@
+package ratespiel
+
+import (
+	"fmt"
+	"math/rand/v2"
+)
+
+func Ratespiel() {
+	gesuchteZahl := RandomNum()
+	for i := 0; i < 3; i++ {
+		guess := ReadNumber()
+		if RichtigeZahl(guess, gesuchteZahl) {
+			fmt.Println("Richtig geraten")
+			return
+		} else {
+			fmt.Println("Falsch versuch es nochmal!")
+		}
+	}
+	fmt.Println("Zu viele Versuche die richtige Zahl war", gesuchteZahl, "game over")
+}
+func RichtigeZahl(a int, r int) bool {
+	return a == r
+}
+func ReadNumber() int {
+	var n int
+	fmt.Print("Rate eine Zahl zwischen 1 und 10: ")
+	fmt.Scan(&n)
+	return n
+}
+func RandomNum() int {
+	return rand.IntN(10)
+}
---
+++ MyCreations/5_12_25/test/main.go
@@ -0,0 +1,7 @@
+package main
+
+import ratespiel "inf1-material/MyCreations/5_12_25/Ratespiel"
+
+func main() {
+	ratespiel.Ratespiel()
+}
