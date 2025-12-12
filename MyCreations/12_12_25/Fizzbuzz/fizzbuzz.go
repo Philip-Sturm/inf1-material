cee67c7d363ac403bc4242b52b2afa5b1d44b5fa
Philip <philip_sturm@outlook.de>
2025-12-12 10:40:57 +0100

    finish product

--- MyCreations/12_12_25/Fizzbuzz/fizzbuzz.go
+++ MyCreations/12_12_25/Fizzbuzz/fizzbuzz.go
@@ -3,11 +3,44 @@ package main
 import "fmt"
 
 func main() {
-	fizz_1()
-	fizz_2()
-	fizz_3()
+	//fizz_1()
+	//fizz_2()
+	//fizz_3()
+	var x int
+	fmt.Print("wähle Fizz: ")
+	fmt.Scan(&x)
+	var y int
+	fmt.Print("wähle Buzz: ")
+	fmt.Scan(&y)
+	var z int
+	fmt.Print("wähle Die Länge: ")
+	fmt.Scan(&z)
+	fizz_allgemein(x, y, z)
 }
 
+func fizz_allgemein(x, y, z int) {
+	for i := 1; i < z+1; i++ {
+		if i%x == 0 && i%y == 0 {
+			fmt.Println("fizzbuzz")
+		} else if i%x == 0 {
+			fmt.Println("fizz")
+		} else if i%y == 0 {
+			fmt.Println("buzz")
+		} else {
+			fmt.Println(i)
+		}
+
+	}
+}
+
+//func Abfrage(x,y)int{
+//	var x int
+//	fmt.Print("wähle Fizz")
+//	fmt.Scan(&x)
+//	var y int
+//	fmt.Print("wähle Buzz")
+//	fmt.Scan(&y)
+//}
 func fizz_1() {
 	for i := 1; i < 100; i++ {
 		if i%5 == 0 && i%7 == 0 {
